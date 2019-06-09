05b55120b1bd65870269c5a03901d7ba085c48e0
Caio Faustino <caiofaustino@gmail.com>
2019-06-09 22:52:04 +0200

    Initiate project

---
+++ app/src/main/java/dev/caiofaustino/jabwa/CreateWalletActivity.kt
@@ -0,0 +1,12 @@
+package dev.caiofaustino.jabwa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateWalletActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_wallet)
+    }
+}
